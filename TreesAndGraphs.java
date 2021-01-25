2f2ad571aeede99ea9616786c5d97f0a1a9e8a61
marcelomorales <lmoral10@jhu.edu>
2021-01-25 12:38:23 -0500

    done with majority of graph problems

--- TreesAndGraphs.java
+++ TreesAndGraphs.java
@@ -5,13 +5,76 @@ import java.util.ArrayList;
 import org.graalvm.compiler.graph.Graph;
 import org.graalvm.compiler.graph.Node;
 
-import javax.swing.tree.TreeNode;
 import java.util.LinkedList;
 
 public class TreesAndGraphs {
    public static void main(String[] args) {
       System.out.println("Hello World");
    }
+
+   public class TreeNode {
+      public int data;
+      public TreeNode left, right, parent;
+      private int size = 0;
+
+      public TreeNode(int d) {
+         data = d;
+         size = 1;
+      }
+
+      public void insertInOrder (int d) {
+         if (d <= data) {
+            if (left == null) {
+               setLeftChild(new TreeNode(d));
+            }  else {
+               left.insertInOrder(d);
+            }
+
+
+         } else {
+            if (right == null) {
+               setRightChild(new TreeNode(d));
+            } else {
+               right.insertInOrder(d);
+            }
+
+         }
+         ++size;
+
+      }
+
+      public int size() {
+         return size;
+      }
+
+      private void setLeftChild(TreeNode treeNode) {
+         this.left = left;
+         if (left != null) {
+            left.parent = this;
+         }
+      }
+
+      private void setRightChild(TreeNode treeNode) {
+         this.right = right;
+         if (right != null) {
+            right.parent = this;
+         }
+      }
+
+      public TreeNode find(int d) {
+         if (d == data) {
+            return this;
+         } else if (d <= data) {
+            return left != null ? left.find(d) : null;
+         } else if (d > data) {
+            return right != null ? right.find(d) : null;
+         }
+         return null;
+      }
+   }
+
+
+
 
    /*
  Given a directed graph and two nodes, design an algorithm to find out whether there is a route from S to E
